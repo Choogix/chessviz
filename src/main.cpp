86967b39c015e61e5bd03c586839b30e7791eb1d
anatoly <tolya_ageev@bk.ru>
2020-04-15 18:39:15 +0700

    Change main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ int main()
     char turn[6];
     int h = 0;
     int move1 = 0, move2 = 0;
+
     char pole[9][9]
             = {{'1', 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
                {'2', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
@@ -20,7 +21,6 @@ int main()
                {'7', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
                {'8', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
                {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}};
-    Chessplace(pole);
 
     while (1) {
         move_p(turn, h, move1, move2, pole);
