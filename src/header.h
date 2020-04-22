a1956a678779fc477fa3c6eef1fd1f8e8f882721
anatoly <tolya_ageev@bk.ru>
2020-04-22 20:57:50 +0700

    Add new file header.h

--- src/header.h
+++ src/header.h
@@ -1,7 +1,16 @@
-#ifndef ADD_H
-#define ADD_H
+#ifndef BOARD_H
+#define BOARD_H
 
-void move_p(char turn[], int h, int move1, int move2, char pole[][9]);
-void Chessplace(char[][9]);
+#include <iomanip>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+void Chessplace(char pole[][9]);
+void move_p(char pole[][9]);
+int move_check(int x1, int x2, int y1, int y2, char pole[][9]);
 
 #endif
